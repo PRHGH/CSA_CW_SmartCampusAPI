ab94c8069162ae2ccba16d2057fdf7fd6df6f620
Pamindu Henndige <pamindurh@gmail.com>
2026-04-12 19:18:30 +0530

    project set up

---
+++ src/main/java/com/mycompany/csa_cw_smartcampusapi/config/AppConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csa_cw_smartcampusapi.config;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author pamin
+ */
+    
+@ApplicationPath("/api/v1")
+public class AppConfig extends Application{
+    //asdadad
+}
